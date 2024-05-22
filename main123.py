925349b560027285f6635f71a79986524867a925
mrlis <mrlis1231.groot@gmail.com>
2024-05-22 15:56:48 +0300

    УРА

--- main123.py
+++ main123.py
@@ -20,40 +20,31 @@ token = "6607529150:AAHnO24RneV49PNkhZdGSdKf8VKFL3c0-9c"
 def send_welcome(message):
     bot.send_message(message.chat.id, 'Hi i gpt 35 turbo on russian'.format(message.from_user))
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 
 logfile = str(datetime.date.today()) + '.log'
 
 
 def audio_to_text(dest_name: str):
-# Function for translation of audio, in the format .vaw in the text.
     r = sr.Recognizer()
     # Read our .vaw file.
     message = sr.AudioFile(dest_name)
     with message as source:
         audio = r.record(source)
-    result = r.recognize_google(audio, language="ru_RU") # Here you can change the recognition language.
+    result = r.recognize_google(audio, language="ru_RU")
     return result
 
 @bot.message_handler(content_types=['voice'])
 def get_audio_messages(message):
-# The main function that takes a voice from the user.
     try:
         print("Started recognition...")
-        # Below we try to extract the name of the file, and in general we take data from the Telegram message.
         file_info = bot.get_file(message.voice.file_id)
-        path = os.path.splitext(file_info.file_path)[0] # This is the full path to the file (for example: voice/file_2.oga)
-        fname = os.path.basename(path) # We convert the path to the file name (for example: file_2.oga)
-        # We get and save the sent voice message.
-        # The admin can at any time turn off the removal of audio files and listen to everything that you say there.
-        # Imagine that this is hooked into a huge chat and it will simply logging all messages [anonymity in a telegram, lol])
+        path = os.path.splitext(file_info.file_path)[0]
+        fname = os.path.basename(path)
         doc = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
         with open(fname+'.oga', 'wb') as f:
-            f.write(doc.content) # Here the audio-message is saved.
-        process = subprocess.run(['ffmpeg', '-i', fname+'.oga', fname+'.wav']) # It is used here ffmpeg to convert .oga in .vaw.
-        result = audio_to_text(fname+'.wav') # Call the function for transferring audio to text, and at the same time we transfer the names of files for their subsequent deletion.
+            f.write(doc.content)
+        process = subprocess.run(['ffmpeg', '-i', fname+'.oga', fname+'.wav'])
+        result = audio_to_text(fname+'.wav')
         bot.send_message(message.chat.id, GPT4(format(result)))
     except sr.UnknownValueError as e:
         bot.send_message(message.chat.id, "Прошу прощения, но я не разобрал сообщение или же оно пустое...")
